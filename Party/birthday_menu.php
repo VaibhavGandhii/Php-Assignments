a316742a42e2383a9494c1ed35988123970d3d1c
VaibhavGandhii <54982167+VaibhavGandhii@users.noreply.github.com>
2019-10-09 22:22:56 +0530

    Add files via upload

---
+++ Party/birthday_menu.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<style type="text/css">
+		#t1
+		{
+			float: right;
+			border-top: 2px solid black;
+			border-bottom: 2px solid black;
+			border-left:2px solid black;
+			border-right:2px solid black;
+			box-shadow: 10px 5px 5px black;
+			margin-top: -558px;
+			height: 500px;
+			margin-right: 55px;
+			background-color: lightgreen;
+		}
+		body
+		{
+			background-image: url(wp2976210.jpg);
+			background-repeat: no-repeat;
+			background-size: cover;
+		}
+		
+	</style>
+</head>
+<body>
+	<center><h2><u>Choose from the items below:</u></h2><br>
+	<form>
+		<table>
+		<tr><th>CAKES : </th><td><input type="checkbox" name="checkbox">Chocolate<br>
+			<input type="checkbox" name="checkbox">Black Forest<br>
+			<input type="checkbox" name="checkbox">Vanilla<br>
+			<input type="checkbox" name="checkbox">Strawberry<br>
+			<input type="checkbox" name="checkbox">Red Velvet<br>
+			<input type="checkbox" name="checkbox">Dark Choco<br>
+			<input type="checkbox" name="checkbox">Fruit Cake<br>
+			<input type="checkbox" name="checkbox">Blueberry<br></td></tr>
+			<tr><td><hr></td></tr>
+		<tr><th>DRINKS: </th><td><input type="checkbox" name="checkbox">Blueberry<br>
+			<input type="checkbox" name="checkbox">Mojito<br>
+			<input type="checkbox" name="checkbox">Shakes<br>
+			<input type="checkbox" name="checkbox">Juice<br>
+			<input type="checkbox" name="checkbox">Coffee<br>
+			<input type="checkbox" name="checkbox">Soft drinks<br>
+			<input type="checkbox" name="checkbox">Hard drinks<br>
+			<input type="checkbox" name="checkbox">Mocktails<br></td></tr>
+			<tr><td><hr></td></tr>
+		<tr><th>SNACKS :</th><td><input type="checkbox" name="checkbox">Sandwiches<br>
+			<input type="checkbox" name="checkbox">Burger<br>
+			<input type="checkbox" name="checkbox">Pizza<br>
+			<input type="checkbox" name="checkbox">Pasta<br>
+			<input type="checkbox" name="checkbox">Honey Chilly<br>
+			<input type="checkbox" name="checkbox">Sausage Rolls<br>
+			<input type="checkbox" name="checkbox">Bread Roll<br>
+			<input type="checkbox" name="checkbox">Noodles<br>
+
+		</td></tr>
+
+		<tr><td></td><td><input type="submit" name="submit"></td></tr>
+
+	</form>
+</center>
+<form>
+<div >
+	<table id="t1">
+		
+		<tr><th>Cake :</th><td> <input type="text" name="text" placeholder="Any other option you would like to order!"></td></tr>/<br>
+		<tr><th>Drink : </th><td><input type="text" name="drink" placeholder="Any other"></td></tr><br>
+		<tr><th>Snacks : </th><td><input type="text" name="snacks"><br></td></tr>
+		<tr><th>Feedback :</th><td><textarea rows="6" cols="25"></textarea></td></tr>
+
+		<tr><td><input type="submit" name="submit"></td></tr>
+	</table>
+</div>
+</form>
+</body>
+</html>
---
+++ Party/surprise_menu.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link href="https://fonts.googleapis.com/css?family=Great+Vibes&display=swap" rel="stylesheet">
+	<style type="text/css">
+		#t1
+		{
+			float: right;
+			border-top: 2px solid black;
+			border-bottom: 2px solid black;
+			border-left:2px solid black;
+			border-right:2px solid black;
+			box-shadow: 10px 5px 5px black;
+			margin-top: -600px;
+			height: 500px;
+			margin-right: 55px;
+			background-color: lightblue;
+		}
+		body
+		{
+			background-image: url(b6ff56380b10dc78f6b1da969b4c48ee.jpg);
+			background-repeat: no-repeat;
+			background-size: cover;
+			color: Red;
+		}
+		h1
+		{
+			 font-family: 'Great Vibes', cursive;
+			 float: left;
+			 font-size: 100px;
+			 text-shadow: 2px 2px 8px #FFFFFF
+		}
+		
+	</style>
+</head>
+<body>
+	
+	<center><h2 style="color: black"><u>Choose from the items below:</u></h2><br>
+		<h1>Menu for <br> Surprise Party<br></h1>
+	<form>
+		<table style="color: blue;text-shadow: 1px 1px #FF0000; ">
+		<tr><th>CAKES : </th><td><input type="checkbox" name="checkbox">Chocolate<br>
+			<input type="checkbox" name="checkbox">Black Forest<br>
+			<input type="checkbox" name="checkbox">Vanilla<br>
+			<input type="checkbox" name="checkbox">Strawberry<br>
+			<input type="checkbox" name="checkbox">Red Velvet<br>
+			<input type="checkbox" name="checkbox">Dark Choco<br>
+			<input type="checkbox" name="checkbox">Fruit Cake<br>
+			<input type="checkbox" name="checkbox">Bluberry<br></td></tr>
+			<tr><td><hr></td></tr>
+		<tr><th>DRINKS: </th><td><input type="checkbox" name="checkbox">Blueberry<br>
+			<input type="checkbox" name="checkbox">Mojito<br>
+			<input type="checkbox" name="checkbox">Shakes<br>
+			<input type="checkbox" name="checkbox">Juice<br>
+			<input type="checkbox" name="checkbox">Coffee<br>
+			<input type="checkbox" name="checkbox">Soft drinks<br>
+			<input type="checkbox" name="checkbox">Hard Drinks<br>
+			<input type="checkbox" name="checkbox">Mocktails<br></td></tr>
+			<tr><td><hr></td></tr>
+			<tr><th>DECORATION	: </th><td><input type="checkbox" name="checkbox">Candles<br>
+			<input type="checkbox" name="checkbox">Flowers<br>
+			<input type="checkbox" name="checkbox">Lightings<br>
+			<input type="checkbox" name="checkbox">Balloons<br>
+			<input type="checkbox" name="checkbox">Handcrafts/origami<br>
+			<input type="checkbox" name="checkbox">Room Plants<br>
+			<input type="checkbox" name="checkbox">Paintings<br></td></tr>
+			<tr><td><hr></td></tr>
+		<tr><th>SNACKS :</th><td><input type="checkbox" name="checkbox">Sandwiches<br>
+			<input type="checkbox" name="checkbox">Burger<br>
+			<input type="checkbox" name="checkbox">Pizza<br>
+			<input type="checkbox" name="checkbox">Pasta<br>
+			<input type="checkbox" name="checkbox">Honey Chilly<br>
+			<input type="checkbox" name="checkbox">Sausage Rolls<br>
+			<input type="checkbox" name="checkbox">Bread Roll<br>
+			<input type="checkbox" name="checkbox">Noodles<br>
+
+		</td></tr>
+
+		<tr><td></td><td><input type="submit" name="submit"></td></tr>
+
+	</form>
+</center>
+<form>
+<div >
+	
+	<table id="t1">
+		
+		<tr><th>Gift item :</th><td><textarea rows="6" cols="25"></textarea></td></tr>
+		<tr><th>Feedback :</th><td><textarea rows="6" cols="25"></textarea></td></tr>
+
+		<tr><td><input type="submit" name="submit"></td></tr>
+	</table>
+</div>
+</form>
+</body>
+</html>
